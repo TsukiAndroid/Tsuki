cf9f6c9206cfdaab1b2992b2c93647745d1f53ce
Zephyros <149416573+Space4414@users.noreply.github.com>
2026-05-03 12:04:38 +0600

    fix: remove stale action_downloads/incognito/settings refs from MainMenuProvider

--- app/src/main/kotlin/io/github/landwarderer/futon/main/ui/MainMenuProvider.kt
+++ app/src/main/kotlin/io/github/landwarderer/futon/main/ui/MainMenuProvider.kt
@@ -8,42 +8,25 @@ import io.github.landwarderer.futon.R
 import io.github.landwarderer.futon.core.nav.AppRouter
 
 class MainMenuProvider(
-	private val router: AppRouter,
-	private val viewModel: MainViewModel,
+        private val router: AppRouter,
+        private val viewModel: MainViewModel,
 ) : MenuProvider {
 
-	override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-		menuInflater.inflate(R.menu.opt_main, menu)
-	}
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.opt_main, menu)
+        }
 
-	override fun onPrepareMenu(menu: Menu) {
-		menu.findItem(R.id.action_incognito)?.isChecked =
-			viewModel.isIncognitoModeEnabled.value
-		val hasAppUpdate = viewModel.appUpdate.value != null
-		menu.findItem(R.id.action_app_update)?.isVisible = hasAppUpdate
-	}
+        override fun onPrepareMenu(menu: Menu) {
+                val hasAppUpdate = viewModel.appUpdate.value != null
+                menu.findItem(R.id.action_app_update)?.isVisible = hasAppUpdate
+        }
 
-	override fun onMenuItemSelected(menuItem: MenuItem): Boolean = when (menuItem.itemId) {
-		R.id.action_settings -> {
-			router.openSettings()
-			true
-		}
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean = when (menuItem.itemId) {
+                R.id.action_app_update -> {
+                        router.openAppUpdate()
+                        true
+                }
 
-		R.id.action_incognito -> {
-			viewModel.setIncognitoMode(!menuItem.isChecked)
-			true
-		}
-
-		R.id.action_app_update -> {
-			router.openAppUpdate()
-			true
-		}
-
-		R.id.action_downloads -> {
-			router.openDownloads()
-			true
-		}
-
-		else -> false
-	}
+                else -> false
+        }
 }
