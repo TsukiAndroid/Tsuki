1cd2b49d9b95a1864ead15c9a686b238cc2ded84
Zephyros <149416573+Space4414@users.noreply.github.com>
2026-09-06 16:23:49 +0600

    Add files via upload

--- app/src/main/kotlin/io/github/landwarderer/futon/settings/about/AboutSettingsFragment.kt
+++ app/src/main/kotlin/io/github/landwarderer/futon/settings/about/AboutSettingsFragment.kt
@@ -68,13 +68,10 @@ class AboutSettingsFragment : BasePreferenceFragment(R.string.about) {
 
                         AppSettings.KEY_LINK_DISCORD -> {
                                 openLink(R.string.url_discord, preference.title)
+                                true
                         }
 
                         AppSettings.KEY_LINK_TELEGRAM -> {
-                                openLink(R.string.url_discord, preference.title)
-                        }
-
-
                                 if (!openLink(R.string.url_telegram, null)) {
                                         openLink(R.string.url_telegram_web, preference.title)
                                 }
